553121b087328cd0335dac94de7981299beb5f45
JohanaSab <johis0826@gmail.com>
2025-02-19 22:42:18 -0500

    Lista_Chequeo_Seg_Actualizacion.py

--- Lista_Chequeo_Seg_Actualizacion.py
+++ Lista_Chequeo_Seg_Actualizacion.py
@@ -7,6 +7,7 @@ import pandas as pd
 from datetime import date
 import streamlit as st
 import requests
+from io import BytesI0
 
 # Estilo para tonos azules y logo
 st.markdown(
@@ -277,13 +278,11 @@ def reiniciar_formulario():
 # Función para cargar la base de datos desde un archivo Excel
 @st.cache_data
 def load_data():
-    url = "https://raw.github.com/JohanaSab/proyectos/main/DIRECTORIO_Operadores.xlsx"
-    response = requests.get(url)
+    url = "https://raw.githubusercontent.com/JohanaSab/proyectos/main/DIRECTORIO_Operadores.xlsx"
+     response = requests.get(url)
     if response.status_code == 200:
-        with open("temp.xlsx", "wb") as f:
-            f.write(response.content)  # Guarda el archivo temporalmente
-
-        return pd.read_excel("temp.xlsx")  # Lee el archivo descargado
+        excel_data = BytesIO(response.content)  # Convierte la respuesta en un objeto BytesIO
+        return pd.read_excel(excel_data)
     else:
         st.error(f"Error al descargar el archivo: {response.status_code}")
         return None
@@ -291,7 +290,8 @@ def load_data():
 df = load_data()
 
 if df is not None:
-    st.write(df)  # Muestra los datos en Streamlit
+    st.write("Datos cargados:")
+    st.write(df)
 else:
     st.warning("No se pudo cargar la base de datos.")
     
